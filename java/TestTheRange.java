8ed1df9d8fc9bad9b8d4b53f1d17d905dfa0e012
Namita <aroranamita33@gmail.com>
2019-03-06 14:27:11 +0530

    completed

--- java/TestTheRange.java
+++ java/TestTheRange.java
@@ -1,19 +1,8 @@
  package com.stackroute;
  class TestTheRange {
-     int checkTomJerry(int num) {
-        if (num >= 20 && num <= 30) {
-            if (num % 2 != 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        return 2;
+     /*WRITE YOUR CODE HERE*/
     }
     String checkItOut(int num) {
-        if (checkTomJerry(num) == 1)
-            return "Tom";
-         else if (checkTomJerry(num) == 0) return "Jerry";
-         else return "Not Found";
+        /*WRITE YOUR CODE HERE*/
     }
 }
